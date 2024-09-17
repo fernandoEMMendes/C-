91c3daa358568ce59d119cfee3c0a4faa14e40ef
Fernando Mendes <126703540+fernandoEMMendes@users.noreply.github.com>
2024-09-16 21:33:52 -0300

    Add files via upload

---
+++ Advinha2segunda/Advinha2segunda/Program.cs
@@ -0,0 +1,75 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+
+int vida2 = 300;
+int vida1 = 100;
+int pocao = 3;
+int dmg1 = 0;
+int dmg2 = 0;
+
+int healActive = 0;
+
+do {
+    Console.Clear();
+
+    Console.WriteLine("\n\n");
+    if (dmg1 != 0 || dmg2 != 0) { Console.WriteLine($"\t\t -{dmg2} \t\t\t\t   -{dmg1}"); dmg1 = 0; dmg2 = 0; }
+    if (healActive == 1) { Console.WriteLine("\t\t +25"); healActive = 0; }
+
+    Console.WriteLine($"\tvida: {vida1} \t\t\tchefão: {vida2}");
+    Console.WriteLine($"\n\tA = ataque");
+    Console.WriteLine($"\t  D = poção({pocao} restantes)");
+
+    ConsoleKeyInfo key = Console.ReadKey();
+
+    if (key.Key == ConsoleKey.A)
+    {
+        /*
+        Attack();
+        EnemyAttack();
+        vida1 = vida1 - dmg2;
+        vida2 = vida2 - dmg1;
+        */
+    }
+    
+    else if (key.Key == ConsoleKey.D)
+    {
+        if (pocao > 0 && vida1 < 100)
+        {
+            Heal();
+        } else
+        {
+            continue;
+        }
+    }
+
+    if (vida1 <= 0) { vida1 = 0; Console.Clear(); Console.WriteLine("\n\n\t\tvocê perdeu!"); break; }
+    if (vida2 <= 0) { vida2 = 0; Console.Clear(); Console.WriteLine("\n\n\t\tvocê ganhou!"); break; }
+   
+} while (vida1 > 0 || vida2 > 0);
+
+int EnemyAttack()
+{
+    Random n2 = new Random();
+    dmg2 = Convert.ToInt32(n2.Next(1, 5));
+    return dmg2;
+}
+
+int Attack()
+{
+    Random n1 = new Random();
+    dmg1 = Convert.ToInt32(n1.Next(1, 10));
+    return dmg1;
+}
+
+int Heal()
+{
+    pocao--;
+    vida1 = vida1 + 25;
+    if (vida1 > 100)
+    {
+        vida1 = 100;
+    }
+    healActive = 1;
+    return vida1;
+}
---
+++ testeRPG/Program.cs
@@ -0,0 +1,75 @@
+﻿namespace testeRPG
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int times = 0;
+            int[] hits = new int[5];
+
+            while (times <= 4) { 
+                int retorno = NewAttack();
+                Console.Clear();
+                hits[times] = retorno;
+
+                times++;
+            }
+
+            for (int i = 0; i <= 4; i++)
+                Console.WriteLine(hits[i]);
+        }
+
+        static int NewAttack()
+        {
+            ConsoleKey[] test = [ConsoleKey.UpArrow, ConsoleKey.LeftArrow, ConsoleKey.RightArrow, ConsoleKey.DownArrow];
+            Random random = new Random();
+            int pad1 = Convert.ToInt32(random.Next(0, 4));
+            int pad2 = Convert.ToInt32(random.Next(0, 4));
+            int pad3 = Convert.ToInt32(random.Next(0, 4));
+            int pad4 = Convert.ToInt32(random.Next(0, 4));
+            int pad5 = Convert.ToInt32(random.Next(0, 4));
+
+            int[] numbers = [pad1, pad2, pad3, pad4, pad5];
+            string[] numbersText = new string[5];
+
+            for (int i = 0; i <= 4; i++)
+            {
+                if (numbers[i] == 0)
+                    numbersText[i] = "CIMA";
+                else if (numbers[i] == 1)
+                    numbersText[i] = "ESQUERDA";
+                else if (numbers[i] == 2)
+                    numbersText[i] = "DIREITA";
+                else if (numbers[i] == 3)
+                    numbersText[i] = "BAIXO";
+            }
+
+            for (int i = 0; i <= 4; i++)
+            Console.WriteLine($"{numbersText[i]}");
+
+
+            ConsoleKeyInfo input1 = Console.ReadKey();
+            ConsoleKeyInfo input2 = Console.ReadKey();
+            ConsoleKeyInfo input3 = Console.ReadKey();
+            ConsoleKeyInfo input4 = Console.ReadKey();
+            ConsoleKeyInfo input5 = Console.ReadKey();
+
+            ConsoleKeyInfo[] inputs = [input1, input2, input3, input4, input5];
+
+            int count = 0;
+
+            for (int i = 0; i <= 4; i++)
+            {
+
+                if (inputs[i].Key == test[numbers[i]]);
+                {
+                    count++;
+                }
+            }
+            /*
+           Todos os acertos estão vindo como certos, não há como errar
+            */
+            return count;
+        }
+    }
+}
